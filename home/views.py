7a6fc1754b886f87757f85c746b2658759dd04f7
Uras Ayanoglu <uras@ayanoglu.org>
2024-12-18 13:00:25 +0200

    Add home view to render the home page with latest announcements and background images

--- home/views.py
+++ home/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from .models import Announcement, BackgroundImage
 
-# Create your views here.
+
+def home(request):
+    """
+    Renders the home page of the application.
+
+    Fetches the latest announcements and all background images to be displayed
+    on the home page.
+
+    Context:
+        announcements (QuerySet): The latest 5 announcements ordered by date in descending order.
+        background_images (QuerySet): All background images for the home page.
+
+    Template:
+        home/home.html
+    """
+    # Fetch latest 5 announcements ordered by date in descending order.
+    announcements = Announcement.objects.order_by('-date')[:5]
+
+    # Fetch all background images for the home page.
+    background_images = BackgroundImage.objects.all()
+
+    # Render the home page with the context data.
+    return render(request, 'home/home.html', {
+        'announcements': announcements,
+        'background_images': background_images,
+    })
