139d7c73cc3dd5b9b33c565040592b80b345f6ed
Uras Ayanoglu <uras@ayanoglu.org>
2025-01-08 11:30:26 +0200

    Register aiserver app in settings.py

--- emblab/settings.py
+++ emblab/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'home', # home app
     'users', # users app
+    'aiserver', # aiserver app
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
